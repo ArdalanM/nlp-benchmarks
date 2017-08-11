cc1503536d087b0227fe0457b3942ffa387b200a
ArdalanM <ardalan77400@gmail.com>
2017-08-11 16:47:40 +0200

    add CNN

---
+++ experiments/CNN/CNN_amazon_review_small.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+cd ../../
+dataset="amazon_review"
+model_folder="models/CNN/CNN_${dataset}_small"
+epoch_size=30000
+batch_size=128
+iterations=$(($epoch_size*50))
+halving=$((3*$epoch_size))
+
+python -m src.CNN   --dataset "${dataset}" \
+                    --model_folder "${model_folder}" \
+                    --maxlen 1014 \
+                    --batch_size ${batch_size} \
+                    --test_batch_size ${batch_size} \
+                    --test_interval ${epoch_size} \
+                    --iterations ${iterations} \
+                    --lr 0.01 \
+                    --lr_halve_interval ${halving} \
+                    --seed 1337 \
+                    --small_config \
+                    --gpu
---
+++ experiments/CNN/run_all.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+./CNN_imdb_small.sh
+./CNN_ag_news_small.sh
+./CNN_sogu_news_small.sh
+./CNN_db_pedia_small.sh
+./CNN_yelp_polarity_small.sh
+./CNN_yelp_review_small.sh
+./CNN_yahoo_answer_small.sh
+./CNN_amazon_review_small.sh
+./CNN_amazon_polarity_small.sh
