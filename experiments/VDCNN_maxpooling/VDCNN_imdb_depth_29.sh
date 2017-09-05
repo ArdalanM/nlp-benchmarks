ad1b717ed22bf0543964912724e21bbb867d64ed
ArdalanM <ardalan77400@gmail.com>
2017-09-05 15:34:10 +0200

    new experiments: VDCNN with max pooling (instead of k-max-pooling)

---
+++ experiments/VDCNN_maxpooling/VDCNN_imdb_depth_29.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+cd ../../
+dataset="imdb"
+depth=29
+model_folder="models/VDCNN_maxpooling/VDCNN_maxpooling_${dataset}_depth@${depth}"
+epoch_size=195
+batch_size=128
+iterations=$(($epoch_size*50))
+halving=$((3*$epoch_size))
+
+python -m src.VDCNN --dataset "${dataset}" \
+                    --model_folder "${model_folder}" \
+                    --depth ${depth} \
+                    --maxlen 1024 \
+                    --chunk_size 2048 \
+                    --batch_size ${batch_size} \
+                    --test_batch_size ${batch_size} \
+                    --test_interval ${epoch_size} \
+                    --iterations ${iterations} \
+                    --lr 0.01 \
+                    --lr_halve_interval ${halving} \
+                    --seed 1337 \
+                    --last_pooling_layer 'max-pooling' \
